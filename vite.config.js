fe25784dffeb1089fd2cb72d008369f587a07313
Travis Kool <koolsuccess@gmail.com>
2025-04-15 20:14:25 -0700

    mve index to src

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist'
+  },
   base: '/',
   plugins: [react()]
 })
